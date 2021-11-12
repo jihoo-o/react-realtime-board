567b0f44d9a0452dea66aa6aeaa35ee74cde5452
Liz <seonhwakei@gmail.com>
2021-11-12 15:15:09 +0900

    handle state change on editor

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,8 +7,8 @@ import Editor from '../editor/editor';
 import Preview from '../preview/preview';
 
 const Maker = ({ authService }) => {
-    const [cards, setCards] = useState([
-        {
+    const [cards, setCards] = useState({
+        1: {
             id: '1',
             name: 'Liz',
             company: 'toss',
@@ -19,29 +19,42 @@ const Maker = ({ authService }) => {
             fileName: 'alalal',
             fileUrl: null,
         },
-        {
-            id: '2',
-            name: 'Liz2',
-            company: 'toss',
-            theme: 'Light',
-            title: 'Software Enginner',
-            email: 'liza@gmail.com',
-            message: 'hi',
-            fileName: 'alalal',
-            fileUrl: 'liz.png',
-        },
-        {
-            id: '3',
-            name: 'Liz3',
-            company: 'toss',
-            theme: 'Colorful',
-            title: 'Software Enginner',
-            email: 'liza@gmail.com',
-            message: 'hi',
-            fileName: 'alalal',
-            fileUrl: null,
-        },
-    ]);
+    });
+    // const [cards, setCards] = useState([
+    //     {
+    //         id: '1',
+    //         name: 'Liz',
+    //         company: 'toss',
+    //         theme: 'Dark',
+    //         title: 'Software Enginner',
+    //         email: 'liza@gmail.com',
+    //         message: 'hi',
+    //         fileName: 'alalal',
+    //         fileUrl: null,
+    //     },
+    //     {
+    //         id: '2',
+    //         name: 'Liz2',
+    //         company: 'toss',
+    //         theme: 'Light',
+    //         title: 'Software Enginner',
+    //         email: 'liza@gmail.com',
+    //         message: 'hi',
+    //         fileName: 'alalal',
+    //         fileUrl: 'liz.png',
+    //     },
+    //     {
+    //         id: '3',
+    //         name: 'Liz3',
+    //         company: 'toss',
+    //         theme: 'Colorful',
+    //         title: 'Software Enginner',
+    //         email: 'liza@gmail.com',
+    //         message: 'hi',
+    //         fileName: 'alalal',
+    //         fileUrl: null,
+    //     },
+    // ]);
     const history = useHistory();
 
     const onLogout = () => {
@@ -60,17 +73,32 @@ const Maker = ({ authService }) => {
         });
     });
 
-    const addCard = (card) => {
-        console.log(card);
-        const updated = [...cards, card];
-        setCards(updated);
+    const createOrUpdateCard = (card) => {
+        setCards((cards) => {
+            const updated = { ...cards };
+            updated[card.id] = card;
+            return updated;
+        });
+    };
+
+    const deleteCard = (card) => {
+        setCards((cards) => {
+            const updated = { ...cards };
+            delete updated[card.id];
+            return updated;
+        });
     };
 
     return (
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor cards={cards} addCard={addCard} />
+                <Editor
+                    cards={cards}
+                    addCard={createOrUpdateCard}
+                    updateCard={createOrUpdateCard}
+                    deleteCard={deleteCard}
+                />
                 <Preview cards={cards} />
             </div>
             <Footer />
