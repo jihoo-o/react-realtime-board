3deea1fad7c5920a016b2b95ded111d080bf1d55
Liz <seonhwakei@gmail.com>
2021-11-26 10:20:31 +0900

    settings for review

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import styles from './app.module.css';
-import Login from './components/login/login';
-import Maker from './components/maker/maker';
 
-function App({ FileInput, authService, cardRepository }) {
-    return (
-        <div className={styles.app}>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/">
-                        <Login authService={authService} />
-                    </Route>
-                    <Route path="/maker">
-                        <Maker
-                            FileInput={FileInput}
-                            authService={authService}
-                            cardRepository={cardRepository}
-                        />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
-    );
+function App() {
+    return <h1>hello world</h1>;
 }
 
 export default App;
